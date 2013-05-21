b73e85edbbda8f39bff9ce71db556398ca3d8f87
Lee337 <lee@redroot.io>
2013-05-22 01:32:30 +0200

    made coloumn left align by default

--- lib/slither/column.rb
+++ lib/slither/column.rb
@@ -1,7 +1,8 @@
 require 'date'
 
 class Slither
-  class ParserError < RuntimeError; end
+  class ParserError < RuntimeError;
+  end
 
   class Column
     attr_reader :name, :length, :alignment, :type, :padding, :precision, :default_value, :options
@@ -11,7 +12,7 @@ class Slither
       @name = name
       @length = length
       @options = options
-      @alignment = options[:align] || :right
+      @alignment = options[:align] || :left
       @type = options[:type] || :string
       @padding = options[:padding] || :space
       @default_value = options[:default_value]
@@ -39,7 +40,8 @@ class Slither
           else
             Date.strptime(value)
           end
-        else value.strip
+        else
+          value.strip
       end
     rescue
       raise ParserError, "Error parsing column ''#{name}'. The value '#{value}' could not be converted to type #{@type}: #{$!}"
@@ -53,84 +55,84 @@ class Slither
 
     private
 
-      def formatter
-        "%#{aligner}#{sizer}s"
-      end
+    def formatter
+      "%#{aligner}#{sizer}s"
+    end
 
-      def aligner
-        @alignment == :left ? '-' : ''
-      end
+    def aligner
+      @alignment == :left ? '-' : ''
+    end
 
-      def sizer
-        (@type == :float && @precision) ? @precision : @length
-      end
+    def sizer
+      (@type == :float && @precision) ? @precision : @length
+    end
 
-      def apply_default(value)
-        value.to_s.empty? ? @default_value : value
-      end
+    def apply_default(value)
+      value.to_s.empty? ? @default_value : value
+    end
 
-      # Manually apply padding. sprintf only allows padding on numeric fields.
-      def pad(value)
-        return value unless @padding == :zero
-        matcher = @alignment == :right ? /^ +/ : / +$/
-        space = value.match(matcher)
-        return value unless space
-        value.gsub(space[0], '0' * space[0].size)
-      end
+    # Manually apply padding. sprintf only allows padding on numeric fields.
+    def pad(value)
+      return value unless @padding == :zero
+      matcher = @alignment == :right ? /^ +/ : / +$/
+      space = value.match(matcher)
+      return value unless space
+      value.gsub(space[0], '0' * space[0].size)
+    end
 
-      def inspect
-        "#<#{self.class} #{instance_variables.map{|iv| "#{iv}=>#{instance_variable_get(iv)}"}.join(', ')}>"
+    def inspect
+      "#<#{self.class} #{instance_variables.map { |iv| "#{iv}=>#{instance_variable_get(iv)}" }.join(', ')}>"
+    end
+
+    def to_s(value)
+      result = case @type
+                 when :date
+                   # If it's a DBI::Timestamp object, see if we can convert it to a Time object
+                   unless value.respond_to?(:strftime)
+                     value = value.to_time if value.respond_to?(:to_time)
+                   end
+                   if value.respond_to?(:strftime)
+                     if @options[:format]
+                       value.strftime(@options[:format])
+                     else
+                       value.strftime
+                     end
+                   else
+                     value.to_s
+                   end
+                 when :float
+                   @options[:format] ? @options[:format] % value.to_f : value.to_f.to_s
+                 when :money
+                   "%.2f" % value.to_f
+                 when :money_with_implied_decimal
+                   "%d" % (value.to_f * 100)
+                 else
+                   value.to_s
+               end
+      validate_size result
+    end
+
+    def assert_valid_options(options)
+      unless options[:align].nil? || [:left, :right].include?(options[:align])
+        raise ArgumentError, "Option :align only accepts :right (default) or :left"
       end
-      
-      def to_s(value)
-        result = case @type
-          when :date
-            # If it's a DBI::Timestamp object, see if we can convert it to a Time object
-            unless value.respond_to?(:strftime)
-              value = value.to_time if value.respond_to?(:to_time)
-            end
-            if value.respond_to?(:strftime)
-              if @options[:format]
-                value.strftime(@options[:format])
-              else
-                value.strftime
-              end
-            else
-              value.to_s
-            end
-          when :float
-            @options[:format] ? @options[:format] % value.to_f : value.to_f.to_s
-          when :money
-            "%.2f" % value.to_f
-          when :money_with_implied_decimal
-            "%d" % (value.to_f * 100)
-          else
-            value.to_s
+      unless options[:padding].nil? || [:space, :zero].include?(options[:padding])
+        raise ArgumentError, "Option :padding only accepts :space (default) or :zero"
+      end
+    end
+
+    def validate_size(result)
+      # Handle when length is out of range
+      if result.length > @length
+        if @truncate
+          start = @alignment == :left ? 0 : -@length
+          result = result[start, @length]
+        else
+          raise Slither::FormattedStringExceedsLengthError,
+                "The formatted value '#{result}' in column '#{@name}' exceeds the allowed length of #{@length} chararacters."
         end
-        validate_size result
       end
-
-      def assert_valid_options(options)
-        unless options[:align].nil? || [:left, :right].include?(options[:align])
-          raise ArgumentError, "Option :align only accepts :right (default) or :left"
-        end
-        unless options[:padding].nil? || [:space, :zero].include?(options[:padding])
-          raise ArgumentError, "Option :padding only accepts :space (default) or :zero"
-        end
-      end
-
-      def validate_size(result)
-        # Handle when length is out of range
-        if result.length > @length
-          if @truncate
-            start = @alignment == :left ? 0 : -@length
-            result = result[start, @length]
-          else
-            raise Slither::FormattedStringExceedsLengthError,
-              "The formatted value '#{result}' in column '#{@name}' exceeds the allowed length of #{@length} chararacters."
-          end
-        end
-        result
-      end
+      result
+    end
   end
 end
--- lib/slither/slither.rb
+++ lib/slither/slither.rb
@@ -1,36 +1,43 @@
 class Slither
-  
+
   VERSION = '0.99.5'
-  
-  class DuplicateColumnNameError < StandardError; end
-  class RequiredSectionNotFoundError < StandardError; end
-  class RequiredSectionEmptyError < StandardError; end
-  class FormattedStringExceedsLengthError < StandardError; end
-  class ColumnMismatchError < StandardError; end
-  class LineWrongSizeError < StandardError; end
-  class SectionsNotSameLengthError < StandardError; end
-  
-  
+
+  class DuplicateColumnNameError < StandardError;
+  end
+  class RequiredSectionNotFoundError < StandardError;
+  end
+  class RequiredSectionEmptyError < StandardError;
+  end
+  class FormattedStringExceedsLengthError < StandardError;
+  end
+  class ColumnMismatchError < StandardError;
+  end
+  class LineWrongSizeError < StandardError;
+  end
+  class SectionsNotSameLengthError < StandardError;
+  end
+
+
   def self.define(name, options = {}, &block)
     definition = Definition.new(options)
     yield(definition)
     definitions[name] = definition
     definition
   end
-  
+
   def self.generate(definition_name, data)
     definition = definition(definition_name)
     raise ArgumentError, "Definition name '#{definition_name}' was not found." unless definition
     generator = Generator.new(definition)
     generator.generate(data)
   end
-  
+
   def self.write(filename, definition_name, data)
     File.open(filename, 'w') do |f|
-      f.write generate(definition_name, data)  
+      f.write generate(definition_name, data)
     end
   end
-  
+
   def self.parse(filename, definition_name)
     raise ArgumentError, "File #{filename} does not exist." unless File.exists?(filename)
 
@@ -44,14 +51,14 @@ class Slither
     parser = Parser.new(definition, io)
     definition.options[:by_bytes] ? parser.parse_by_bytes : parser.parse
   end
-  
+
   private
-  
-    def self.definitions
-      @@definitions ||= {}
-    end
-    
-    def self.definition(name)
-      definitions[name]
-    end
+
+  def self.definitions
+    @@definitions ||= {}
+  end
+
+  def self.definition(name)
+    definitions[name]
+  end
 end
